46445236684f232a922a1086c03942311b0a8941
VicoKev <kevinpatinvoh@gmail.com>
2026-04-09 20:06:10 +0100

    feat: add description field to permissions and roles tables for enhanced clarity

--- database/migrations/2026_04_09_180511_create_permission_tables.php
+++ database/migrations/2026_04_09_180511_create_permission_tables.php
@@ -28,6 +28,7 @@ return new class extends Migration
         Schema::create($tableNames['permissions'], static function (Blueprint $table) {
             $table->uuid('uuid')->primary()->unique(); // permission id
             $table->string('name');
+            $table->string('description');
             $table->string('guard_name');
             $table->timestamps();
 
@@ -44,6 +45,7 @@ return new class extends Migration
                 $table->index($columnNames['team_foreign_key'], 'roles_team_foreign_key_index');
             }
             $table->string('name');
+            $table->string('description');
             $table->string('guard_name');
             $table->timestamps();
             if ($teams || config('permission.testing')) {
